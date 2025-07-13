301ce9364e88b43041c78d85aab7677e55bb69f2
Pranay Tuladhar <pranaytuladhar101@gmail.com>
2025-07-13 18:40:02 +0545

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include"Game.hpp"
+Game *game = nullptr;
+int main(int argc,char* argv[])
+{
+  game = new Game();
+  game->init("First Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,800,true);
+
+
+  while (game->running())
+  {
+    game->handleEvents();
+    game->update();
+    game->render();
+  }
+
+  game->clean();
+  
+  return 0;
+}
