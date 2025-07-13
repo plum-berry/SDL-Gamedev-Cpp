5e72eecb96cb8681670bb7fb83b32c8235068c03
Pranay Tuladhar <pranaytuladhar101@gmail.com>
2025-07-13 18:48:54 +0545

    arranged .dll files

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ Game *game = nullptr;
 int main(int argc,char* argv[])
 {
   game = new Game();
-  game->init("First Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,800,true);
+  game->init("First Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,800,false);
 
 
   while (game->running())
